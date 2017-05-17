b07c4c276bfb1cd9def1f5a8fe6a1048b40d92fb
yf w <yf w>
2017-05-17 14:01:59 +0800

    改进1代码放到8实验内容/experiment3/code_改进1

--- 8.实验内容/experiment3/code_改进1/dupefilters.py
+++ 8.实验内容/experiment3/code_改进1/dupefilters.py
@@ -7,8 +7,7 @@ import logging
 from scrapy.utils.job import job_dir
 from scrapy.utils.request import request_fingerprint
 from filters import Filters
-
-#from pybloomfilter import BloomFilter
+from mongodb_agent import MongoDBClient
 
 
 class BaseDupeFilter(object):
@@ -32,51 +31,58 @@ class BaseDupeFilter(object):
 class RFPDupeFilter(BaseDupeFilter):
     """Request Fingerprint duplicates filter"""
 
-    def __init__(self, m_length, error_rate, path=None, debug=False):
-        self.file = None
+    def __init__(self, m_length, error_rate, db_host, db_port, \
+                 dbname, dbcollections, path=None, debug=False):
         self.logdupes = True
         self.debug = debug
         self.logger = logging.getLogger(__name__)
-        if path:
-            filepath = os.path.join(path, 'requests.seen')
-            if os.path.exists(filepath):
-                self.fingerprints = Filters(m_length, error_rate)
-                self.logger.info("created bloomfilter({},{}) from exist file {}"
-                        .format(m_length, error_rate, filepath))
-            else:
-                self.fingerprints = Filters(m_length, error_rate)
-                self.logger.info("created bloomfilter({},{}) from new file {}"
-                        .format(m_length, error_rate, filepath))
-        else:
-            self.fingerprints = Filters(m_length, error_rate)
-            self.logger.info("created bloomfilter({},{}) from anonymous file"
-                    .format(m_length, error_rate))
+
+        self.fingerprints = Filters(m_length, error_rate)
+        self.logger.info("created bloomfilter({},{}) <----- wangyf"\
+                .format(m_length, error_rate))
+
+        self.mongodb = MongoDBClient(db_host, db_port, dbname, dbcollections)
 
     @classmethod
     def from_settings(cls, settings):
         debug = settings.getbool('DUPEFILTER_DEBUG')
         m_length = settings['MAX_LENGTH']
         error_rate = settings['ERROR_RATE']
-        return cls(m_length, error_rate, job_dir(settings), debug)
+        mongo_host = settings['DB_HOST']
+        mongo_port = settings['DB_PORT']
+
+        try:
+            dbname = settings['DB_NAME']
+            dbcollections = settings['DB_COLLECTIONS_NAME']
+        except Exception:
+            dbname = None
+            dbcollections = None
+
+        return cls(m_length, error_rate, mongo_host, mongo_port,\
+                   dbname, dbcollections, job_dir(settings), debug)
 
     def request_seen(self, request):
         fp = self.request_fingerprint(request)
-        if fp in self.fingerprints:
+        if fp in self.fingerprints or fp in self.mongodb:
+            self.logger.info("filtered request at {}  <-- wangyf".format(request.url))
             return True
+
         self.fingerprints.add(fp)
+        return False
 
     def request_fingerprint(self, request):
-        return request_fingerprint(request)
+        return request.url
 
     def close(self, reason):
+        self.mongodb.close()
         return
 
     def log(self, request, spider):
         if self.debug:
-            msg = "BloomFilter filtered duplicate request: %(request)s"
+            msg = "DupeFilter filtered duplicate request: %(request)s"
             self.logger.debug(msg, {'request': request}, extra={'spider': spider})
         elif self.logdupes:
-            msg = ("BloomFilter filtered duplicate request: %(request)s"
+            msg = ("DupeFilter filtered duplicate request: %(request)s"
                    " - no more duplicates will be shown"
                    " (see DUPEFILTER_DEBUG to show all duplicates)")
             self.logger.debug(msg, {'request': request}, extra={'spider': spider})
@@ -84,62 +90,5 @@ class RFPDupeFilter(BaseDupeFilter):
 
         spider.crawler.stats.inc_value('dupefilter/filtered', spider=spider)
 
-
-'''
-class RFPDupeFilter(BaseDupeFilter):
-    """Request Fingerprint duplicates filter"""
-
-    def __init__(self, m_length, error_rate, path=None, debug=False):
-        self.file = None
-        self.logdupes = True
-        self.debug = debug
-        self.logger = logging.getLogger(__name__)
-        if path:
-            filepath = os.path.join(path, 'requests.seen')
-            if os.path.exists(filepath):
-                self.fingerprints = BloomFilter.open(filepath)
-                self.logger.info("created bloomfilter({},{}) from exist file {}"
-                        .format(m_length, error_rate, filepath))
-            else:
-                self.fingerprints = BloomFilter(m_length, 0.0001, filepath)
-                self.logger.info("created bloomfilter({},{}) from new file {}"
-                        .format(m_length, error_rate, filepath))
-        else:
-            self.fingerprints = BloomFilter(m_length, 0.0001, None)
-            self.logger.info("created bloomfilter({},{}) from anonymous file"
-                    .format(m_length, error_rate))
-
-    @classmethod
-    def from_settings(cls, settings):
-        debug = settings.getbool('DUPEFILTER_DEBUG')
-        m_length = settings['MAX_LENGTH']
-        error_rate = settings['ERROR_RATE']
-        return cls(m_length, error_rate, job_dir(settings), debug)
-
-    def request_seen(self, request):
-        fp = self.request_fingerprint(request)
-        if fp in self.fingerprints:
-            return True
-        self.fingerprints.add(fp)
-
-    def request_fingerprint(self, request):
-        return request_fingerprint(request)
-
-    def close(self, reason):
-        return
-
-    def log(self, request, spider):
-        if self.debug:
-            msg = "BloomFilter filtered duplicate request: %(request)s"
-            self.logger.debug(msg, {'request': request}, extra={'spider': spider})
-        elif self.logdupes:
-            msg = ("BloomFilter filtered duplicate request: %(request)s"
-                   " - no more duplicates will be shown"
-                   " (see DUPEFILTER_DEBUG to show all duplicates)")
-            self.logger.debug(msg, {'request': request}, extra={'spider': spider})
-            self.logdupes = False
-
-        spider.crawler.stats.inc_value('dupefilter/filtered', spider=spider)
-'''
 
 
